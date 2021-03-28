e12a065bbd900a3d21b6b73441212cffb84dadec
CherkashinD <cherkashin.du@gmail.com>
2021-03-28 18:18:10 +0300

    add project

---
+++ Async/Async/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var text = ConcatTextAsync().GetAwaiter().GetResult();
+            Console.WriteLine(text);
+        }
+
+        private static Task<string> ReadFileAsync()
+        {
+            return File.ReadAllTextAsync("Hello.txt");
+        }
+
+        private static Task<string> WordAsync()
+        {
+            return Task.Run(() => "World");
+        }
+
+        private static async Task<string> ConcatTextAsync()
+        {
+            var readFileTask = ReadFileAsync();
+            var wordTask = WordAsync();
+            var result = $"{await readFileTask} {await wordTask}";
+            return result;
+        }
+    }
+}
